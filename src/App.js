08124955d163c1192334331ba1aa158b12c700a7
YuukiEuphoria <144450582+YuukiEuphoria@users.noreply.github.com>
2023-10-04 17:58:02 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import UsersPage from './pages/UsersPage/UsersPage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <UsersPage />
+
+    </div>
+  );
+}
+
+export default App;
